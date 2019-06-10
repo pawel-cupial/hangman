7d245309e54fd6c724a5766efcac228da4a59bf9
Paweł Cupiał <p.cupial@outlook.com>
2019-06-10 19:07:55 +0200

    add setStatusMessage()

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const puzzlePar = document.querySelector('#puzzle')
 const guessesPar = document.querySelector('#guesses')
 
 puzzlePar.textContent = word1.getPuzzle()
-guessesPar.textContent = word1.guesses
-console.log(word1.status)
+guessesPar.textContent = word1.setStatusMessage()
 
 window.addEventListener('keypress', (e) => {//Event odpalany jest przy każdym naciśnięciu klawisza w oknie przeglądarki
     const guess = String.fromCharCode(e.charCode)//Zamienia kod nacisniętego klawisza na jego faktyczną wartość
     word1.makeGuess(guess)
     puzzlePar.textContent = word1.getPuzzle()
-    guessesPar.textContent = word1.guesses
-    console.log(word1.status)
+    guessesPar.textContent = word1.setStatusMessage()
 })
 
--- main.js
+++ main.js
@@ -18,6 +18,16 @@ Hangman.prototype.setStatus = function() {
     }
 }
 
+Hangman.prototype.setStatusMessage = function() {
+    if (this.status === 'playing') {
+        return `Guesses left: ${this.guesses}`
+    } else if (this.status === 'failed') {
+        return `Nice try! The word was: ${this.word.join('')}`
+    } else if (this.status === 'finished') {
+        return `Great work! You guessed the word!`
+    }
+}
+
 /*Poniższa metoda zwraca słowo-zagadkę. Funkcja iteruje przez całe słowo, jeśli w gussedLetters zawarta jest podana przez użytkownika litera,
 to zwracana jest ona jako widoczna, jeśli podanej litery nie ma w gussedLetters zwracana jest '*'. Widoczne są również spacje (|| letter === ' ')*/
 Hangman.prototype.getPuzzle = function() {
@@ -36,6 +46,10 @@ Hangman.prototype.makeGuess = function(guess) {
     guess = guess.toLowerCase()
     const isUnique = !this.guessedLetters.includes(guess)//Spawrdza, czy litera podana przez użytkownika znajduje się w tablicy guessedLetters
     const isGoodGuess = this.word.includes(guess)//Sprawdza, czy podana przez użytkownika litera znajduje się w słowie-zagadce
+
+    if (this.status !== 'playing') {
+        return//Zwraca undefined dzięki czemu jeśłi status jest inny niż playing, to kod niżej nie ruszy i nie będą przyjmowane dalsze guesses
+    }
 
     if(isUnique) {
         this.guessedLetters.push(guess)
